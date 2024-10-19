92a5a816e7ce08ee841f9bd779aa737b6aaa8641
Adryll <enderstorm100@gmail.com>
2024-10-19 13:04:56 +0100

    lololol i hate my life

--- algoritmos.py
+++ algoritmos.py
@@ -3,7 +3,7 @@ from sokoban import *
 from ProblemaGrafoHs import *
 
 
-def beam_search_plus_count(problem, W, f):
+def beam_search_plus_count_jack(problem, W, f):
     """Beam Search: search the nodes with the best W scores in each depth.
        Return the solution and how many nodes were expanded."""
     f = memoize(f, 'f')
@@ -21,6 +21,7 @@ def beam_search_plus_count(problem, W, f):
     it's not in vain"""
     while frontier:
         node = frontier.pop()
+        print(node.solution())
         explored.add(node.state)    # "explored" just means judging if it's the end
         if(problem.goal_test(node.state)):  # Cave Johnson, we're done here?
             return node, len(explored)
@@ -46,6 +47,50 @@ def get_W_best (queue, W, f):
         if len(queue) > 0:
             freeman.append(queue.pop())
     return freeman
+
+def beam_search_plus_count(problem, W, f):
+    """Beam Search: search the nodes with the best W scores in each depth.
+       Return the solution and how many nodes were expanded."""
+    f = memoize(f, 'f')
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):   # start from root
+        return node, 0
+    frontier = PriorityQueue(min, f)    # frontier is a priorityqueue of the ones that might be interesting to check
+    frontier.append(node)
+
+    explored = set()    # to keep track of all explored; fast check to not waste time in parsing for comparisons
+    visited_not_explored = {node.state} # to compare similar states from different paths
+
+    """frontier will be destroyed over time as we check all possibile states along
+    the heuristic in f; when it's all over, return how many attempts were made so
+    it's not in vain"""
+    while frontier:
+        newFrontier = PriorityQueue(min, f)
+        while frontier:
+            node = frontier.pop()
+            explored.add(node.state)    # "explored" just means judging if it's the end
+            print (node.solution())
+            print (W)
+            if(problem.goal_test(node.state)):  # Cave Johnson, we're done here?
+                return node, len(explored)
+            if (node.state in visited_not_explored):
+                visited_not_explored.remove(node.state) # literally just explored this state, move on
+            for child in node.expand(problem):
+                if child.state not in explored: # not explored means we add to the list
+
+                    if child.state not in visited_not_explored:
+                        newFrontier.append(child)
+                        visited_not_explored.add(child.state)
+                    else:
+                        if child in newFrontier:
+                            incumbent = newFrontier[child]
+                            if f(child) < f(incumbent):
+                                del newFrontier[incumbent]
+                                newFrontier.append(child)
+        frontier = get_W_best(newFrontier, W, f)   # purge unwanted children
+    return None, len(explored)
+            
+    
 
 def beam_search(problem, W, h=None):
     """Beam graph search with f(n) = g(n)+h(n).
--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 from algoritmos import *
 from sokoban import *
 
+p=ProblemaGrafoHs()
+res, exp = beam_search(p,2,p.h1)
+print(exp)
+if res==None:
+    print('Nope')
+else:
+    print(res.path_cost)
+
+linha1="##########\n"
+linha2="#........#\n"
+linha3="#..$..+..#\n"
+linha4="#........#\n"
+linha5="##########\n"
+mundoS=linha1+linha2+linha3+linha4+linha5
+s=Sokoban(situacaoInicial=mundoS)
+res, exp = beam_search(s,3,s.h_inutil_2)
+print (exp)
+if res:
+    print(res.solution())
+else:
+    print('No solution!')
+
+ 	
+
+linha1="##########\n"
+linha2="#........#\n"
+linha3="#..$..+..#\n"
+linha4="#........#\n"
+linha5="##########\n"
+mundoS=linha1+linha2+linha3+linha4+linha5
+s=Sokoban(situacaoInicial=mundoS)
+res, exp = beam_search(s,3,s.h_inutil_2)
+if res:
+    print(res.solution())
+else:
+    print('No solution!')
+
+
 linha1="##########\n"
 linha2="#........#\n"
 linha3="#..$..+..#\n"
